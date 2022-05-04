39b5c63435578f30fa009725bb7dc9504cdd8f87
Hexuan Li <lihexuan1@gmail.com>
2022-05-04 14:49:05 -0700

    update model index.js

--- models/index.js
+++ models/index.js
@@ -11,16 +11,21 @@ Product.belongsTo(Category, {
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
 })
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany( Tag, { 
-  through: ProductTag,
+  through:  {
+  model: ProductTag,
+  unique: false,
+  },
   foreignKey: 'product_id',
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, { 
-  through: ProductTag,
+  through: {
+  model: ProductTag,
+  unique: false,
+  },
   foreignKey: 'tag_id',
 })
 
