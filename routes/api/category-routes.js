9384dfb44e58a5f04e6eacb3298182c63c319cba
Hexuan Li <lihexuan1@gmail.com>
2022-05-03 20:42:07 -0700

    update model associations

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,6 +4,11 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+  Category.findAll( {
+    include: [{model: Product}],
+  })
+  .then(categoryData => res.json(categoryData))
+  .catch
   // find all categories
   // be sure to include its associated Products
 });
